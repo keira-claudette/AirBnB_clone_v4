467e48fb0a8a356e4f84ce7c41de59078babd7c8
keira-claudette <claudette.keira@gmail.com>
2021-09-25 18:43:14 +0000

    initial commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ from flasgger.utils import swag_from
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
@@ -32,7 +32,7 @@ def not_found(error):
 
 app.config['SWAGGER'] = {
     'title': 'AirBnB clone Restful API',
-    'uiversion': 3
+    'uiversion': 4
 }
 
 Swagger(app)
