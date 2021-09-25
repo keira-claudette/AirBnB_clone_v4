e6a2078c9f98bc9aad23f7a3f80a6f9251513e05
musangisilvia <musangisilvia@gmail.com>
2021-09-25 23:01:36 +0300

    add task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,16 @@
 $( document ).ready(function() {
-    let amenity_list = [];
-    if($('input[type="checkbox"]').prop('checked')){
-	amenity_list.push($(this).attr('data-id'));
-	console.log($(this).attr('data-id'))
-    } else {
-	amenity_list.splice(amniety_list.indexof($(this).attr('data-id')), 1);
-    }
-    if (amenity_list.length() > 0) {
-	$("DIV H4").text(...amenity_list);
-    }
+    let amenity_id = [];
+    let amenity_name = [];
+    $('input[type="checkbox"]').change(function() {
+        if (this.checked) {
+            amenity_id.push($(this).attr('data-id'));
+            amenity_name.push($(this).attr('data-name'));
+        } else {
+            amenity_id.splice(amenity_id.indexOf($(this).attr('data-id')),1);
+            amenity_name.splice(amenity_name.indexOf($(this).attr('data-name')),1);
+        }
+        if (amenity_id.length > 0) {
+            $("DIV.amenities H4").text(amenity_name.join(', '));
+        }
+    });
 });
